c1e21f519386c22c1f8d7b8f4e8194ef72093dd0
Elvin <elvintaghiyev184@gmail.com>
2020-05-24 13:55:47 +0400

    second

--- src/main/java/org/tinder_proj/HerokuEnv.java
+++ src/main/java/org/tinder_proj/HerokuEnv.java
@@ -9,7 +9,6 @@ public class HerokuEnv {
 
   public static int port() {
     try {
-      System.out.println(System.getenv(PORT));
       return Integer.parseInt(System.getenv(PORT));
     } catch (NumberFormatException ex) {
       return 5000;
@@ -17,26 +16,20 @@ public class HerokuEnv {
   }
 
   public static String jdbc_url() {
-    System.out.println(System.getenv(JDBC_DATABASE_URL));
     String url = System.getenv(JDBC_DATABASE_URL);
     if (url == null) throw new IllegalArgumentException(JDBC_DATABASE_URL + EMPTY);
     return url;
   }
 
   public static String jdbc_username() {
-    System.out.println(System.getenv(JDBC_DATABASE_USERNAME));
     String url = System.getenv(JDBC_DATABASE_USERNAME);
     if (url == null) throw new IllegalArgumentException(JDBC_DATABASE_USERNAME + EMPTY);
     return url;
   }
 
   public static String jdbc_password() {
-    System.out.println(System.getenv(JDBC_DATABASE_PASSWORD));
     String url = System.getenv(JDBC_DATABASE_PASSWORD);
     if (url == null) throw new IllegalArgumentException(JDBC_DATABASE_PASSWORD + EMPTY);
     return url;
   }
-
-//  jdbc:postgresql://ec2-54-247-78-30.eu-west-1.compute.amazonaws.com:5432/d3du1hdp316o87
-//  jdbc:postgresql://<host>:<port>/<dbname>?sslmode=require&user=<username>&password=<password>
 }
